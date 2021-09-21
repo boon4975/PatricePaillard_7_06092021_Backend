8de7e758f318ebcb8418f1935a87bf62edd26d84
patrice paillard <paillard.patrice@gmail.com>
2021-09-21 16:27:04 +0200

    route signup

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,9 @@
 const db = require('../config/db.config');
 const bcrypt = require('bcrypt');
+const { password } = require('../config/env');
 const User = db.user;
 
-exports.signup = (req, res) => {
+exports.signup2 = (req, res) => {
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
       User.create({
@@ -18,4 +19,26 @@ exports.signup = (req, res) => {
       .catch(error => res.status(401).json({ error }))
     })
     .catch(error => res.status(500).json({ error }))
+}
+
+exports.signup = (req, res) => {
+  bcrypt.hash(req.body.password, 10)
+  .then(hash => {
+    User.findOrCreate({
+      where: { email: req.body.email },
+      defaults: {
+        email: req.body.email,
+        password: hash,
+        moderator: 1
+      }
+    })
+    .then(result => {
+      if(result[1]){
+        res.status(201).json(result);
+      }else{
+        res.status(200).json(result[0].email + ' existe déjà');
+      }
+    })
+  })
+  .catch(error => res.status(401).json({ error }))
 }
