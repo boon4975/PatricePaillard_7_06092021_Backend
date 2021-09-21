090d8c7c5159907d460da975ada89340197966cc
patrice paillard <paillard.patrice@gmail.com>
2021-09-21 16:41:33 +0200

    signup + regex

--- controllers/user.js
+++ controllers/user.js
@@ -1,44 +1,37 @@
 const db = require('../config/db.config');
 const bcrypt = require('bcrypt');
-const { password } = require('../config/env');
 const User = db.user;
 
-exports.signup2 = (req, res) => {
-    bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-      User.create({
-          email: req.body.email,
-          password: hash,
-          moderator: 1
-      })
-      .then( (user) => {
-          res.status(200).json({ 
-              status: true,
-          message: 'user créé'})
-      })
-      .catch(error => res.status(401).json({ error }))
-    })
-    .catch(error => res.status(500).json({ error }))
-}
+const regexEmail = /^[a-z0-9][a-z0-9._-]+@[a-z0-9._-]{2,}\.([a-z]{2,4})$/;
+const regexPass = /((?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[.:'!@#$%&*_+=|(){}[?\-\]\/\\])(?!.*[<>`])).{8,}/;
 
 exports.signup = (req, res) => {
-  bcrypt.hash(req.body.password, 10)
-  .then(hash => {
-    User.findOrCreate({
-      where: { email: req.body.email },
-      defaults: {
-        email: req.body.email,
-        password: hash,
-        moderator: 1
-      }
-    })
-    .then(result => {
-      if(result[1]){
-        res.status(201).json(result);
-      }else{
-        res.status(200).json(result[0].email + ' existe déjà');
-      }
-    })
-  })
-  .catch(error => res.status(401).json({ error }))
+  let inputEmail = req.body.email;
+  if(inputEmail.match(regexEmail)){
+    if(req.body.password.match(regexPass)){
+      bcrypt.hash(req.body.password, 10)
+      .then(hash => {
+        User.findOrCreate({
+          where: { email: req.body.email },
+          defaults: {
+            email: req.body.email,
+            password: hash,
+            moderator: 1
+          }
+        })
+        .then(result => {
+          if(result[1]){
+            res.status(201).json(result);
+          }else{
+            res.status(200).json(result[0].email + ' est déjà utilisé');
+          }
+        })
+      })
+      .catch(error => res.status(401).json({ error }))
+    }else{
+      return res.status(401).json({ message: 'Le mot de passe doit comporter au moins 8 caractères dont 1 chiffre, 1 minuscule, 1 majuscule, 1 caractère spéciale '})
+  }
+  }else{
+    return res.status(401).json({ message: 'Adresse email non valide'});
+  }
 }
